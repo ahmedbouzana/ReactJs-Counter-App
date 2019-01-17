64867479556993f1f5c63de89156bbbdc9afc810
ahmedbouzana <ah.bouzana@gmail.com>
2019-01-17 11:47:10 +0100

    BuildApp

---
+++ build/precache-manifest.367bd69fa0260918c49f2a85c57e5866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9757b2c5254d560d01ba",
+    "url": "/static/js/main.9757b2c5.chunk.js"
+  },
+  {
+    "revision": "1b80a66936ddd366a98d",
+    "url": "/static/js/1.1b80a669.chunk.js"
+  },
+  {
+    "revision": "9757b2c5254d560d01ba",
+    "url": "/static/css/main.89c334e2.chunk.css"
+  },
+  {
+    "revision": "1b80a66936ddd366a98d",
+    "url": "/static/css/1.9bf11709.chunk.css"
+  },
+  {
+    "revision": "cbca579ce6470570e465e82d9905a3ff",
+    "url": "/index.html"
+  }
+];
